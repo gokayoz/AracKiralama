db57fd9403222352c9fbe52d9afb048d52402728
gokayoz <gokay.oz@outlook.com.tr>
2025-03-16 12:27:09 +0300

    Adds Aracipi enum

---
+++ AracKiralama.UI/Models/AracTipi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AracKiralama.UI.Models
+{
+    public enum AracTipi
+    {
+        Sedan,
+        SUV,
+        Hatchback,
+        Coupe,
+        Cabriolet,
+        Pickup,
+        Minivan,
+        StationWagon,
+        Electric,
+        Hybrid
+    }
+}
