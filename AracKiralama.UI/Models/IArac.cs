d41799fa2e5d5d2c519c13b4e83f1ef3ee1e4bd1
gokayoz <gokay.oz@outlook.com.tr>
2025-03-16 12:26:24 +0300

    Adds IArac interface and properties

---
+++ AracKiralama.UI/Models/IArac.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AracKiralama.UI.Models
+{
+    public interface IArac
+    {
+        string Marka { get; set; }
+        string Model { get; set; }
+        int ModelYili { get; set; }
+        string Renk { get; set; }
+        int GunlukUcret { get; set; }
+        bool Musaitlik { get; set; }
+        AracTipi AracTipi { get; set; }
+        void AracBilgilerGoster();
+    }
+}
